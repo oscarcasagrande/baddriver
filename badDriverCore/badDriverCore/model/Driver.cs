fba8e7305496558965b3fbfda6d8ae3bcc439d7e
Oscar Casagrande <oscar.casagrande@hotmail.com.br>
2017-08-01 01:19:18 -0300

    initial poc commit

---
+++ badDriverCore/badDriverCore/model/Driver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace badDriverCore.model
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public string Model { get; set; }
+        public string Supplier { get; set; }
+        public string Coloer { get; set; }
+
+        public Driver{}()
+    }
+}
