daf569a59b023b69a01354455a86567500473c7b
Oscar Casagrande <oscar.casagrande@hotmail.com.br>
2017-08-01 23:40:54 -0300

    add User

---
+++ badDriverCore/badDriverCore/domain/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace badDriverCore.domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Nickname { get; set; }
+        public bool Active { get; set; }
+
+        public User() { }
+    }
+}
