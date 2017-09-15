2920a726bebac58c66e2453a0af85675a976be92
Terminal 04 <rafael.redoval@gmail.com>
2017-09-15 15:17:07 -0300

    DriverUpload

---
+++ badDriverCore/badDriverWebMockup/UploadDriver.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace badDriverWebMockup
+{
+    public partial class UploadDriver : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            string filepath = "C:\\Program Files\\Git\\Github\\Baddriver\\badDriverCore\\badDriverWebMockup\\DriversPhotos";
+            HttpFileCollection uploadedFiles = Request.Files;
+
+            for (int i = 0; i < uploadedFiles.Count; i++)
+            {
+                HttpPostedFile userPostedFile = uploadedFiles[i];
+
+                try
+                {
+                    if (userPostedFile.ContentLength > 0)
+                    {
+                        Label1.Text += "<u>File #" + (i + 1) +
+                           "</u><br>";
+                        Label1.Text += "File Content Type: " +
+                           userPostedFile.ContentType + "<br>";
+                        Label1.Text += "File Size: " +
+                           userPostedFile.ContentLength + "kb<br>";
+                        Label1.Text += "File Name: " +
+                           userPostedFile.FileName + "<br>";
+
+                        userPostedFile.SaveAs(filepath + "\\" +
+                           System.IO.Path.GetFileName(userPostedFile.FileName));
+
+                        Label1.Text += "Location where saved: " +
+                           filepath + "\\" +
+                           System.IO.Path.GetFileName(userPostedFile.FileName) +
+                           "<p>";
+                    }
+                }
+                catch (Exception Ex)
+                {
+                    Label1.Text += "Error: <br>" + Ex.Message;
+                }
+            }
+        }
+    }
+}
---
+++ badDriverCore/badDriverWebMockup/UploadDriver.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace badDriverWebMockup {
+    
+    
+    public partial class UploadDriver {
+        
+        /// <summary>
+        /// Controle archivesFileUpload.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload archivesFileUpload;
+        
+        /// <summary>
+        /// Controle UploadButton.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button UploadButton;
+        
+        /// <summary>
+        /// Controle Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+    }
+}
