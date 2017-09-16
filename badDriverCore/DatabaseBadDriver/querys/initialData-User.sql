6143999de77ca1f6004b95d32ba73e2ad1f67db6
Oscar Casagrande <oscar.casagrande@hotmail.com.br>
2017-09-16 15:42:26 -0300

    include initial data for table user

---
+++ badDriverCore/DatabaseBadDriver/querys/initialData-User.sql
@@ -0,0 +1,22 @@
+﻿USE BadDriver
+GO
+SET IDENTITY_INSERT [dbo].[User] ON
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'teste@teste.com.br', N'123456', N'nickname', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'teste@teste.com.br', N'123456', N'bacana', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'teste2@teste.com.br', N'123456', N'nickname2', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'teste3@teste.com.br', N'2c2b0f6-', N'nickname3', 0)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'14343daa-dd4c-4a8b-9477-36b5311f2fbf3@14343daa-dd4c-4a8b-9477-36b5311f2fbf.com.br', N'123456', N'2b56493f-9ee0-4bc5-a788-f620c9498b0b', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'bb391480-1a87-4074-a6fb-67e8d15a0c7f3@bb391480-1a87-4074-a6fb-67e8d15a0c7f.com.br', N'123456', N'68343367-5b64-46ff-a154-5461877e0b05', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'9d43bb52-cfdd-4241-a77e-649d233f83f93@9d43bb52-cfdd-4241-a77e-649d233f83f9.com.br', N'123456', N'38921e4a-a977-434e-a649-c100d31351b2', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'147b29ca-63ad-4e88-8907-71f9c15f72ab3@147b29ca-63ad-4e88-8907-71f9c15f72ab.com.br', N'123456', N'4d3e7490-240c-4602-89b2-189685fd5d43', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'c18ff51c-408c-4360-be9f-570e10a462803@c18ff51c-408c-4360-be9f-570e10a46280.com.br', N'123456', N'ce0d86e2-b23b-4b38-b26c-d6605880f46e', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'bc85f45c-e10b-4b8e-8fe2-254ef2249ce53@bc85f45c-e10b-4b8e-8fe2-254ef2249ce5.com.br', N'123456', N'b26fd606-5895-49d0-8850-cea301ce2e34', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'a30cd1e1-dd43-45b8-8a3d-815b492481123@a30cd1e1-dd43-45b8-8a3d-815b49248112.com.br', N'123456', N'921a40bb-235e-49d1-8801-52fed072e89b', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'c041df93-7d67-4704-a8fc-49d0af02d0b03@c041df93-7d67-4704-a8fc-49d0af02d0b0.com.br', N'123456', N'a31fb4f3-ee3b-4b48-8c3d-54b48ca4daf0', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'6f6d6cf9-3758-4473-b1d2-0f18bf7db8cd3@6f6d6cf9-3758-4473-b1d2-0f18bf7db8cd.com.br', N'123456', N'4f990024-d724-46b6-a69d-37be180172f0', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'5dff1b5e-8026-4c81-8920-bed7a92b2a7d3@5dff1b5e-8026-4c81-8920-bed7a92b2a7d.com.br', N'123456', N'7567f24d-1e3c-4e4c-9337-d3dda3b8ac09', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'be1482d6-21b9-49df-972c-dd3b8d08299a3@be1482d6-21b9-49df-972c-dd3b8d08299a.com.br', N'123456', N'5c40ebcd-b4a2-494d-b980-776ddab25415', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'be1482d6-21b9-49df-972c-dd3b8d08299a3@be1482d6-21b9-49df-972c-dd3b8d08299a.com.br', N'123456', N'5c40ebcd-b4a2-494d-b980-776ddab25415', 1)
+INSERT INTO [dbo].[User] ([Email], [Password], [Nickname], [Active]) VALUES (N'be1482d6-21b9-49df-972c-dd3b8d08299a3@be1482d6-21b9-49df-972c-dd3b8d08299a.com.br', N'123456', N'5c40ebcd-b4a2-494d-b980-776ddab25415', 1)
+SET IDENTITY_INSERT [dbo].[User] OFF
+GO
