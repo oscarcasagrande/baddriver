44b856258e5252546c5e1a884951cda8306c14c0
rafa <rafael.redoval@gmail.com>
2017-10-10 16:04:15 -0300

    adjust Default page.

--- badDriverCore/badDriverWebMockup/Cadastro.aspx.designer.cs
+++ badDriverCore/badDriverWebMockup/Cadastro.aspx.designer.cs
@@ -13,6 +13,24 @@ namespace badDriverWebMockup {
     public partial class Cadastro {
         
         /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Textbox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Textbox1;
+        
+        /// <summary>
         /// UserEmailLabel control.
         /// </summary>
         /// <remarks>
