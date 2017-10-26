23d0c98e5d00b2aece625b4be16cd4c48d552004
rafa <rafael.redoval@gmail.com>
2017-10-26 14:37:54 -0200

    adding password recovery function in login.

--- badDriverCore/badDriverWebMockup/RecuperarSenha.aspx.designer.cs
+++ badDriverCore/badDriverWebMockup/RecuperarSenha.aspx.designer.cs
@@ -10,15 +10,24 @@
 namespace badDriverWebMockup {
     
     
-    public partial class Default {
+    public partial class Denunciar {
         
         /// <summary>
-        /// UploadDriverLink control.
+        /// TextBox1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink UploadDriverLink;
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
