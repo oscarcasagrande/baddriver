98169ef24456ac50f3281a7e005d053716547836
RafaelRedoval <rafael.redoval@gmail.com>
2017-10-28 12:07:29 -0200

    List of top ten worst drivers.

---
+++ badDriverCore/badDriverWebMockup/ListWorstDrivers.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="ListWorstDrivers.aspx.cs" Inherits="badDriverWebMockup.ListWorstDrivers" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Panel ID="Panel1" runat="server" Height="128px">
+        <asp:Button ID="Button1" runat="server" Text="Button" align="left" />
+        <asp:ListBox ID="ListBox2" runat="server" Height="168px">
+            <asp:ListItem>Driver1</asp:ListItem>
+            <asp:ListItem>Driver2</asp:ListItem>
+            <asp:ListItem>Driver3</asp:ListItem>
+            <asp:ListItem>Driver4</asp:ListItem>
+            <asp:ListItem>Driver5</asp:ListItem>
+            <asp:ListItem>Driver6</asp:ListItem>
+            <asp:ListItem>Driver7</asp:ListItem>
+            <asp:ListItem>Driver8</asp:ListItem>
+            <asp:ListItem>Driver9</asp:ListItem>
+            <asp:ListItem>Driver10</asp:ListItem>
+        </asp:ListBox>
+        <asp:ListBox ID="ListBox1" runat="server">
+            <asp:ListItem>Board1</asp:ListItem>
+        </asp:ListBox>
+    </asp:Panel>
+</asp:Content>
---
+++ badDriverCore/badDriverWebMockup/ListWorstDrivers.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace badDriverWebMockup {
+    
+    
+    public partial class ListWorstDrivers {
+        
+        /// <summary>
+        /// Controle Panel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel Panel1;
+        
+        /// <summary>
+        /// Controle Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Controle ListBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ListBox2;
+        
+        /// <summary>
+        /// Controle ListBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ListBox1;
+    }
+}
